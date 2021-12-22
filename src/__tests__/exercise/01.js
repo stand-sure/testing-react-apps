71d6c7b6bcbcd4bba34195cc978c54d8eb88d01d
Chris Anderson <chris.anderson@zywave.com>
2021-12-22 14:54:02 -0500

    exercise 1

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -1,26 +1,57 @@
 // simple test with ReactDOM
 // http://localhost:3000/counter
 
-import * as React from 'react'
-import ReactDOM from 'react-dom'
-import Counter from '../../components/counter'
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from '../../components/counter';
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  const div = document.createElement('div');
-  document.body.append(div);
-  ReactDOM.render(<Counter />, div);
+let root = {};
 
-  const [decrement, increment] = document.querySelectorAll('button');
-  const message = div.firstChild.querySelector('div');
-  expect(message.textContent).toBe('Current count: 0');
+function renderComponent(Component) {
+  root = document.createElement('div');
+  document.body.append(root);
+  ReactDOM.render(<Component />, root);
+  return root;
+}
 
-  increment.click();
-  expect(message.textContent).toBe('Current count: 1');
+function getMessageText() {
+  return root.firstChild.querySelector('div').textContent;
+}
 
-  decrement.click();
-  expect(message.textContent).toBe('Current count: 0');
+function click(el) {
+  const event = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  });
 
-  div.remove();
-})
+  el.dispatchEvent(event);
+}
 
-/* eslint no-unused-vars:0 */
+describe('counter increments and decrements when the buttons are clicked', () => {
+  beforeEach(() => {
+    document.body.innerHTML = '';
+    renderComponent(Counter);
+  });
+
+  it("should start at zero", () => {
+    const message = getMessageText();
+    expect(message).toBe('Current count: 0');
+  });
+
+  it("should increment", () => {
+    const [, increment] = document.querySelectorAll('button');
+    click(increment);
+
+    const message = getMessageText();
+    expect(message).toBe('Current count: 1');
+  });
+
+  it("should decrement", () => {
+    const [decrement] = document.querySelectorAll('button');
+    click(decrement);
+
+    const message = getMessageText();
+    expect(message).toBe('Current count: -1');
+  });
+});
